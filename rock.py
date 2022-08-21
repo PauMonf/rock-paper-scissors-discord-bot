3666e9f6a20e364c62205937048c65a91888cd01
PauMonf <97391818+PauMonf@users.noreply.github.com>
2022-08-21 19:50:53 +0200

    rock.py, pero mejor :P
    así si nota menos q hace trampas

--- rock.py
+++ rock.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import random
 
 TOKEN = ""
 
@@ -20,15 +21,34 @@ async def on_ready():
 @bot.command()
 async def play(ctx, input):
     value = str(input)
-    if (value == "rock"):
-        await ctx.send("Paper")
-        await ctx.send("Paper beats rock, I WIN!")
-    if (value == "paper"):
-        await ctx.send("Scissors")
-        await ctx.send("Scissors beats paper, I WIN!")
-    if value == "scissors":
-        await ctx.send("Rock")
-        await ctx.send("Rock beats scissors, I WIN!")
-
+    
+    rand=[0,1]
+    op=random.choice(rand)
+    
+    rock=["rock","ROCK","Rock"]
+    paper=["paper","PAPER","Paper"]
+    scissors=["scissors","SCISSORS","Scissors"]
+    
+    if (value in rock):
+        if (op==0):
+            await ctx.send("Paper")
+            await ctx.send("Paper beats rock, I WIN!")
+        else:
+            await ctx.send("Rock")
+            await ctx.send("It's a tie ╮(￣ω￣;)╭")
+    if (value in paper):
+        if (op==0):
+            await ctx.send("Scissors")
+            await ctx.send("Scissors beats paper, I WIN!")
+        else:
+            await ctx.send("Paper")
+            await ctx.send("It's a tie ╮(￣ω￣;)╭")
+    if (value in scissors):
+        if (op==0):
+            await ctx.send("Rock")
+            await ctx.send("Rock beats scissors, I WIN!")
+        else:
+            await ctx.send("Scissors")
+            await ctx.send("It's a tie ╮(￣ω￣;)╭")
 
 bot.run(TOKEN)
